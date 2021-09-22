c0eaa8a3036b0f52c3995dfa15ae32b4bdb683ff
Brent Pedersen <bpederse@gmail.com>
2021-09-22 11:18:13 +0200

    allow dropping samples

--- src/tiwihpkg/pedbam.nim
+++ src/tiwihpkg/pedbam.nim
@@ -7,7 +7,7 @@ import strutils
 import argparse
 import ./samplename
 
-proc pedbam(samples:var seq[Sample], bam_list:string) =
+proc pedbam(samples:var seq[Sample], bam_list:string, drop_samples_without_bam:bool) =
 
   var t = newTable[string, Sample]()
   for s in samples:
@@ -31,26 +31,31 @@ proc pedbam(samples:var seq[Sample], bam_list:string) =
       path = s["bam_path"]
     except:
       stderr.write_line &"[tiwih pedbam] WARNING: sample in pedigree file: {s.id} not found list of alignment files"
+      if drop_samples_without_bam:
+        continue
       path = ""
 
     echo &"{s.family_id}\t{s.id}\t{s.paternal_id}\t{s.maternal_id}\t{s.sex}\t{s.phenotype}\t{path}"
 
-
 proc pedbam_main*(args:seq[string]=commandLineParams()) =
 
   var p = newParser("pedbam"):
+    flag("-d", "--drop-samples-without-bam", help="if the bam file for a sample in the pedigree is not found, dont output that line")
     arg("ped", nargs=1, help="pedigree/fam file (tab-delimited)")
     arg("bam_list", nargs=1, help="text file containing path to bam/cram files (1 per line)")
 
   try:
     var opts = p.parse(args)
+    if opts.help:
+      quit 0
 
     var samples = parse_ped(opts.ped)
 
-    pedbam(samples, opts.bam_list)
+    pedbam(samples, opts.bam_list, opts.drop_samples_without_bam)
   except UsageError as e:
     stderr.write_line(p.help)
     stderr.write_line(getCurrentExceptionMsg())
     quit 1
+
 when isMainModule:
   pedbam_main()
